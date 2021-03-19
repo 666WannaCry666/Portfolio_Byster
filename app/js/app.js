a4021bda0a29d8e9a81d108f8ac89fb73040f2ec
Dmitry <zeppelin95@yandex.ru>
2021-03-20 00:10:52 +0700

    add border to video, del snow

--- app/js/app.js
+++ app/js/app.js
@@ -77,7 +77,7 @@ function renderVideoContainers(videoList) {
     const videoElement = $(`
       <div>
         <div class="video__slider-item">
-          <div id=${elem} class="video-preview" style="background-image: url(//img.youtube.com/vi/${elem}/maxresdefault.png);" >
+          <div id=${elem} class="video-preview" style="background-image: url(//img.youtube.com/vi/${elem}/maxresdefault.jpg);" >
             <img class="video-preview__btn" src="image/youtube-play-icon.svg">
           </div>
         </div>
