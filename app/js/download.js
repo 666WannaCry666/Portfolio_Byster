55d2e7a8ebd539c8f9adffc51f965876923f71bf
666WannaCry666 <usov3124@gmail.com>
2021-07-08 01:47:14 +0700

    Button Fix. fix #36

--- app/js/download.js
+++ app/js/download.js
@@ -5,21 +5,12 @@ let text = document.getElementById('load-process');
 button.onclick = function() {
     this.style.display = 'none';
     text.style.display = 'block';
-}
-
-// Скролл до инструкции
-const anchor = document.getElementById('load__link')
-
-
-anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-
     document.getElementById('mainTitle').scrollIntoView({
         behavior: 'smooth',
         block: 'start'
-    })
+    });
 
-    document.location.href = "https://byster.ru/";
-})
+}
 
 window.history.pushState(null, null, '/go');
+
