cd35a32ed70aedecf4f1731c34ff6807ab0ae7b8
666WannaCry666 <usov3124@gmail.com>
2021-10-16 14:44:30 +0700

    Подогнал размеры поточнее. fix #52

---
+++ app/jquery_i18n/main-jquery_i18n.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function() {
+    var update_texts = function() { $('body').i18n() };
+    $.i18n().load({
+        'en': {
+            
+          },
+          'ru': {
+            
+          }
+    });
+
+    update_texts();
+
+    
+  });
