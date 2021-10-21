8aac4d6340afa4b2c10bd198557418af021e0f46
666WannaCry666 <usov3124@gmail.com>
2021-10-21 18:42:35 +0700

    Теперь язык сохраняется. fix #55 fix #56

--- app/jquery_i18n/funpay-jquery_i18n.js
+++ app/jquery_i18n/funpay-jquery_i18n.js
@@ -39,19 +39,35 @@ jQuery(document).ready(function() {
 
     let count;
     const lang = navigator.language;
-
-    if (lang === "ru" || lang === "be" || lang === "kk" || lang === "uk") {
-      $.i18n().locale = "ru";
+    
+    if (localStorage.getItem('language')) {
+      $.i18n().locale = localStorage.getItem('language');
       update_texts();
-      $('.lang-switch').html("ENG");
-      count = 0;
+
+      if ($.i18n().locale === "en") {
+        $('.lang-switch').html("RUS");
+        count = 1;
+      }
+      else if ($.i18n().locale === "ru") {
+        $('.lang-switch').html("ENG");
+        count = 0;
+      }
     }
     else {
-      $.i18n().locale = "en";
-      update_texts();
-      $('.lang-switch').html("RUS");
-      count = 1;
+      if (lang === "ru" || lang === "be" || lang === "kk" || lang === "uk") {
+        $.i18n().locale = "ru";
+        update_texts();
+        $('.lang-switch').html("ENG");
+        count = 0;
+      }
+      else {
+        $.i18n().locale = "en";
+        update_texts();
+        $('.lang-switch').html("RUS");
+        count = 1;
+      }
     }
+
     $('.lang-switch').click(function(e) {
         e.preventDefault();
 
@@ -59,12 +75,16 @@ jQuery(document).ready(function() {
             $.i18n().locale = "en";
             count = 1;
             $(this).html("RUS");
+            localStorage.setItem('language', $.i18n().locale);
         }
         else if (count === 1) {
             $.i18n().locale = "ru";
             count = 0;
             $(this).html("ENG");
+            localStorage.setItem('language', $.i18n().locale);
         };
+
+        localStorage.setItem('language', $.i18n().locale);
         update_texts();
     });
   });
